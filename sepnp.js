bd3a106428d36ac30871440fb4134ae446377d55
StephanieOlivares <stephanie.olivares@bryanuniversity.edu>
2020-12-14 09:44:32 -0500

    selecting elements resubmit

--- sepnp.js
+++ sepnp.js
@@ -1,3 +1,9 @@
 var intro = document.getElementById("intro")
 console.log(intro)
 console.dir(intro)
+
+var h = document.createElement("h1"); 
+var t = document.createTextNode("Selecting Elements");
+h.appendChild(t); 
+ 
+document.body.appendChild(h); 
