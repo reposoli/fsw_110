8930b2e8ec1e7e1cbda952129b8266e21c2858f5
reposoli <stephanie.olivares@bryanuniversity.edu>
2020-12-30 14:39:36 -0500

    reattempt

--- week2/creating_appending_elements/creatingelements.js
+++ week2/creating_appending_elements/creatingelements.js
@@ -1,17 +1,12 @@
-document.getElementById("heading").innerHTML = "Welcome to my JS site";
-document.getElementById("paragraph").innerHTML = "All This Was Created With JavaScript";
+var h1 = document.getElementById("heading").innerHTML = "Welcome to my JS site";
+var p = document.getElementById("paragraph").innerHTML = "This Was Created With JavaScript."
 
-var newLi = document.createElement("li")
-newLi.textContent = "item4 created in javascript"
-var myOl = document.getElementById("myOl")
-myOl.append(newLi)
+var myOrder = document.getElementById("myOrder").prepend("An Ordered List")
+document.getElementById("itemOne").innerHTML = "item One";
+document.getElementById("itemTwo").innerHTML = "item Two";
+document.getElementById("itemThree").innerHTML = "item Three";
 
-var newLi = document.createElement("li")
-newLi.textContent = "item5 created in javascript"
-var myOl = document.getElementById("myOl")
-myOl.append(newLi)
 
-var newLi = document.createElement("li")
-newLi.textContent = "item6 created in javascript"
-var myOl = document.getElementById("myOl")
-myOl.append(newLi)
+
+
+
