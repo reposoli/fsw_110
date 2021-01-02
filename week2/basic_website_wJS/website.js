a1fdb52aa17ccd69b2313722c70412e64f2dd0a2
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-01-02 13:32:43 -0500

    first commit

--- week2/basic_website_wJS/website.js
+++ week2/basic_website_wJS/website.js
@@ -1,15 +1,54 @@
-document.getElementById("heading").innerHTML = "AMMOSOUL";
-document.getElementById("paragraph").innerHTML = "Elderberry Infused Sea Moss Gel"
+var a = document.createElement('a');  
+var link = document.createTextNode("Home");
+a.appendChild(link);  
+a.href = "http://www.ammosoul.com/ammosoulartillery.html";  
+document.body.appendChild(a)
 
-document.getElementById("myList").prepend("5 Signature Flavors")
-document.getElementById("itemOne").innerHTML = "Applia";
-document.getElementById("itemTwo").innerHTML = "MaxBerry";
-document.getElementById("itemThree").innerHTML = "Strawbanie";
-document.getElementById("itemFour").innerHTML = "SouLAmmo";
-document.getElementById("itemFive").innerHTML = "BluezBerry";
+var a = document.createElement('a');  
+var link = document.createTextNode("About"); 
+a.appendChild(link);  
+a.href = "http://www.ammosoul.com/ammosoulfaqs.html";  
+document.body.appendChild(a)
 
-var nav = getElementById("home").innerHTML = "home"
-var result = getElementById("home.link")
+var a = document.createElement('a');  
+var link = document.createTextNode("Contact"); 
+a.appendChild(link);  
+a.href = "http://www.ammosoul.com/ammosouldelivery.html";  
+document.body.appendChild(a)
 
-home.link("url").innerHTML = "home"
+var newH1 = document.createElement("h1")
+newH1.textContent = "AMMOSOUL"
+document.body.append(newH1)
+
+var newP = document.createElement("p")
+newP.textContent = "Elderberry Infused Sea Moss gel"
+document.body.append(newP)
+
+var newOl = document.createElement("ol")
+newOl.textContent = "5 Signature Flavors"
+document.body.append(newOl)
+
+var listItem = document.createElement("li")
+listItem.textContent = "BluezBerry"
+newOl.append(listItem)
+
+var listItem = document.createElement("li")
+listItem.textContent = "Applia"
+newOl.append(listItem)
+
+var listItem = document.createElement("li")
+listItem.textContent = "SouLAmmo"
+newOl.append(listItem)
+
+var listItem = document.createElement("li")
+listItem.textContent = "Strawbanie"
+newOl.append(listItem)
+
+var listItem = document.createElement("li")
+listItem.textContent = "MaxBerry"
+newOl.append(listItem)
+
+var footer = document.createElement("footer")
+footer.textContent = "© 2020 - Stephanie Olivares Footer "
+document.body.append(footer)
 
