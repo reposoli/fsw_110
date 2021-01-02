17960dc5f8a8f1837abbeab1af6165824413c9f0
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-01-02 09:12:36 -0500

    resubmit2

---
+++ week2/creating_appending_elements/basic_website_wJS/website.js
@@ -0,0 +1,15 @@
+document.getElementById("heading").innerHTML = "AMMOSOUL";
+document.getElementById("paragraph").innerHTML = "Elderberry Infused Sea Moss Gel"
+
+document.getElementById("myList").prepend("5 Signature Flavors")
+document.getElementById("itemOne").innerHTML = "Applia";
+document.getElementById("itemTwo").innerHTML = "MaxBerry";
+document.getElementById("itemThree").innerHTML = "Strawbanie";
+document.getElementById("itemFour").innerHTML = "SouLAmmo";
+document.getElementById("itemFive").innerHTML = "BluezBerry";
+
+var nav = getElementById("home").innerHTML = "home"
+var result = getElementById("home.link")
+
+home.link("url").innerHTML = "home"
+
--- week2/creating_appending_elements/creatingelements.js
+++ week2/creating_appending_elements/creatingelements.js
@@ -1,12 +1,32 @@
-var h1 = document.getElementById("heading").innerHTML = "Welcome to my JS site";
-var p = document.getElementById("paragraph").innerHTML = "This Was Created With JavaScript."
-
-var myOrder = document.getElementById("myOrder").prepend("An Ordered List")
-document.getElementById("itemOne").innerHTML = "item One";
-document.getElementById("itemTwo").innerHTML = "item Two";
-document.getElementById("itemThree").innerHTML = "item Three";
+var newH1 = document.createElement("h1")
+newH1.textContent = "Welcome to my JS site"
+document.body.append(newH1)
 
 
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with Javascript"
+document.body.append(newP)
 
+var newOl = document.createElement("ol")
+newOl.textContent = "An Ordered List in JS"
+document.body.append(newOl)
 
+var listItem = document.createElement("li")
+listItem.textContent = "Item One"
+newOl.append(listItem)
 
+var listItem = document.createElement("li")
+listItem.textContent = "Item Two"
+newOl.append(listItem)
+
+var listItem = document.createElement("li")
+listItem.textContent = "Item Three"
+newOl.append(listItem)
+
+var listItem = document.createElement("li")
+listItem.textContent = "Item Four"
+newOl.append(listItem)
+
+var listItem = document.createElement("li")
+listItem.textContent = "Item Five"
+newOl.append(listItem)
