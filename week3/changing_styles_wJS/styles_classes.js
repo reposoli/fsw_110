b5eff9d248de506f571404c4c0f5da0c4709ca51
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-01-14 16:48:58 -0500

    first commit

---
+++ week3/changing_styles_wJS/styles_classes.js
@@ -0,0 +1,15 @@
+
+for (newH2 = 0; newH2 < 5; newH2 ++) {
+    const repH2 = document.createElement("h2");
+    repH2.innerHTML = "H2 Elements";
+    repH2.style.fontSize = '20px';                    
+    repH2.style.fontWeight = 'lighter';
+    repH2.style.fontFamily = 'sans-serif';
+    repH2.style.color = 'cornflowerblue';
+    repH2.classList.add("border");
+    document.body.appendChild(repH2);
+}
+
+
+
+
