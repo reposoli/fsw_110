d3f1dbeff63fbe3719e384a0c82002968dc8e2e4
reposoli <stephanie.olivares@bryanuniversity.edu>
2021-01-20 17:11:04 -0500

    first commit

---
+++ week4/DJ_js/dj.js
@@ -0,0 +1,47 @@
+var box = document.createElement("box")
+box.textContent = "Color Changing Box",
+box.style.fontSize = '40px';
+box.classList.add("box");
+document.body.append(box)
+
+
+box.addEventListener("mouseover", function() {
+    box.style.backgroundColor = "blue"
+})
+box.addEventListener("mouseup", function() {
+    box.style.backgroundColor = "yellow"
+})
+box.addEventListener("mousedown", function() {
+    box.style.backgroundColor = "red"
+})
+box.addEventListener("dblclick", function() {
+    box.style.backgroundColor = "green"
+})
+box.addEventListener("wheel", function() {
+    box.style.backgroundColor = "orange"
+})
+
+document.onkeydown = function () {
+    var keyCode = window.event ? window.event.keyCode : event.which;
+    keyColor(keyCode);
+}
+
+function keyColor(keyCode) {
+
+if (keyCode == 66) {
+    box.style.backgroundColor = "blue";
+} else if (keyCode == 82) {
+    box.style.backgroundColor = "red";
+} else if (keyCode == 89) {
+    box.style.backgroundColor = "yellow";
+} else if (keyCode == 71) {
+    box.style.backgroundColor = "green";
+} else if (keyCode == 79) {
+    box.style.backgroundColor = "orange";
+}
+}
+
+
+
+
+
